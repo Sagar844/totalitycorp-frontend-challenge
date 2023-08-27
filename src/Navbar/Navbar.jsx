d2ffeabdca5c1382db2ddaf173698c7ff3a801c5
Vishal-digi360 <99277810+Sagar844@users.noreply.github.com>
2023-08-27 14:11:28 +0530

    logo url

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -3,24 +3,20 @@ import React from "react";
 import { BsCartCheck } from "react-icons/bs";
 import { Link } from "react-router-dom";
 import { withCart } from "../withProvider";
+import logo from "../assets/print-1-1.svg";
 
-function Navbar({totalcount}) {
+function Navbar({ totalcount }) {
   return (
     <div className="bg-white py-3 flex justify-between items-center shadow-md  ">
       <div className="">
         <Link to="/">
-        <img
-          className="ml-5  w-12 max-w-sm"
-          src="/src/assets/print-1-1.svg"
-        />
+          <img className="ml-5  w-12 max-w-sm" src={logo} />
         </Link>
-       
       </div>
-      
+
       <div className=" flex   ">
         <Link to="/products/cart">
-        <BsCartCheck className="text-orange-500 text-xl hover:text-orange-500" />
-
+          <BsCartCheck className="text-orange-500 text-xl hover:text-orange-500" />
         </Link>
         <span className="bg-orange-500 text-white rounded-full h-6 w-6 text-center  mr-5 ">
           {totalcount}
